0bd95616ec58efce4efeeb5699e2607a447f7593
SongYeonSOO <lucklys324@gmail.com>
2017-08-31 15:59:43 +0900

    default UI : Do not use full-screen flag to remain toolbar

--- app/src/main/java/ys/grad/mytalk/StartActivity.java
+++ app/src/main/java/ys/grad/mytalk/StartActivity.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class StartActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_start);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
--- app/src/main/java/ys/grad/mytalk/TalkActivity.java
+++ app/src/main/java/ys/grad/mytalk/TalkActivity.java
@@ -3,12 +3,18 @@ package ys.grad.mytalk;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class TalkActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_talk);
+
+
     }
 }
